ea5aaa568f022cc9c14a880b14ca7fe53ff5458c
sakura1783 <sarukaniumai@gmail.com>
2024-06-03 21:45:47 +0900

    CharacterDataSO.csに処理を追加

--- Assets/Scripts/CharacterDataSO.cs
+++ Assets/Scripts/CharacterDataSO.cs
@@ -1,6 +1,38 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+public enum CharaName
+{
+    Rosevillea,
+    Nina,
+    Elliot,
+    Arilosha,
+    Setsuna,
+}
+
+/// <summary>
+/// 職業
+/// </summary>
+public enum Profession
+{
+    ウォーリア,
+    ガーディアン,
+    ソーサラー,
+}
+
+/// <summary>
+/// 属性
+/// </summary>
+public enum Attribute
+{
+    藍,
+    紅,
+    翠,
+    黄,
+    天,
+    冥,
+}
+
 
 [CreateAssetMenu(fileName = "CharacterDataSO", menuName = "Create CharacterDataSO")]
 public class CharacterDataSO : ScriptableObject
@@ -11,6 +43,19 @@ public class CharacterDataSO : ScriptableObject
     [System.Serializable]
     public class CharacterData
     {
+        public string name;
+        public CharaName englishName;
+        public Profession profession;
+        public Attribute attribute;
+
+        public int defaultCombatPower;
+        public int defaultAttackPower;
+        public int defaultDefencePower;
+        public int defaultHp;
+        public float defaultCriticalRate;
+
+        // TODO 各ステータスの増加率、他のステータス追加
+
 
         /// <summary>
         /// コンストラクタ
@@ -18,7 +63,15 @@ public class CharacterDataSO : ScriptableObject
         /// <param name="datas"></param>
         public CharacterData(string[] datas)
         {
-
+            name = datas[0];
+            englishName = (CharaName)System.Enum.Parse(typeof(CharaName), datas[1]);
+            profession = (Profession)System.Enum.Parse(typeof(Profession), datas[2]);
+            attribute = (Attribute)System.Enum.Parse(typeof(Attribute), datas[3]);
+            defaultCombatPower = int.Parse(datas[4]);
+            defaultAttackPower = int.Parse(datas[5]);
+            defaultDefencePower = int.Parse(datas[6]);
+            defaultHp = int.Parse(datas[7]);
+            defaultCriticalRate = float.Parse(datas[8]);
         }
     }
 }
