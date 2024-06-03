527458619d8afe7f4f18f154d064b3afa95f35dc
sakura1783 <sarukaniumai@gmail.com>
2024-06-03 12:35:55 +0900

    ConstData.cs、CalculateManager.cs、AbstractSingleton.csを追加

---
+++ Assets/Scripts/ConstData.cs
@@ -0,0 +1,9 @@
+/// <summary>
+/// 定数管理用クラス
+/// </summary>
+public static class ConstData
+{
+    // TODO ランク(SRやSSRなど)によって変化させる
+    public static float POWER_INC_RATE = 0.025064f;  // 通常の戦闘力増加率
+    public static float POWER_INC_RATE_BREAK_LIMIT = 0.3f; // 限界突破時の戦闘力増加率
+}
