9d8954b7bfcd1e490fd10d3818428245356d22fc
sakura1783 <sarukaniumai@gmail.com>
2024-06-04 01:22:30 +0900

    CalculateManager.cs、レベルアップ後のキャラのステータス計算用メソッド(CalculateCharaStatusメソッド)を追加。

--- Assets/Scripts/CalculateManager.cs
+++ Assets/Scripts/CalculateManager.cs
@@ -1,11 +1,14 @@
 using System;
 using UnityEngine;
+using static UnityEditorInternal.ReorderableList;
 
 public class CalculateManager : AbstractSingleton<CalculateManager>
 {
     // TODO ConstDataクラスに移動？
-    private const float ATTACK_MODIFIER = 1.8f;  // 攻撃力補正値
-    private const float DEFENCE_MODIFIRE = 2f;  // 防御力補正値
+    private const float ATTACK_MODIFIER = 1.7f;  // 攻撃力補正値
+    private const float DEFENCE_MODIFIRE = 2f;
+    private const float HP_MODIFIRE = 0.08f;
+
 
     private const float ATTRIBUTE_BONUS = 0.25f;  // 属性相性がいい場合、ダメージを25%増加
     private const float CRITICAL_BONUS = 0.5f;  // クリティカル発生時、ダメージを50%増加
@@ -42,28 +45,39 @@ public class CalculateManager : AbstractSingleton<CalculateManager>
     }
 
     /// <summary>
-    /// TODO レベルアップ後の各ステータスの計算
+    /// レベルアップ後の各ステータスの計算
     /// </summary>
-    /// <param name="currentLevel"></param>
+    /// <param name="charaData"></param>
     /// <param name="targetLevel"></param>
     /// <returns></returns>
-    public int CalculateCharaStatus(int currentLevel, int targetLevel)
+    public void CalculateCharaStatus(GameData.OwnedCharaData charaData, int targetLevel)
     {
-        for (int i = currentLevel; i < targetLevel; i++)
+        for (int i = charaData.level; i < targetLevel; i++)
         {
+            // TODO キャラのランクによってステータス上昇率を変更する
+
+            // 限界突破(Lv20→21,40→41 etc...)
             if (i % 20 == 0)
             {
-                // Lv20→21(40→41 etc...)の時だけ、限界突破として、現在の戦闘力の30%を足す
-                //combatPower += combatPower * 0.3f;
+                // 攻撃力、防御力、HPの30%を足す
+                charaData.attackPower += (int)Math.Round(charaData.attackPower * 0.3f, 0, MidpointRounding.AwayFromZero);
+                charaData.defencePower += (int)Math.Round(charaData.defencePower * 0.3f, 0, MidpointRounding.AwayFromZero);
+                charaData.hp += (int)Math.Round(charaData.hp * 0.3f, 0, MidpointRounding.AwayFromZero);
+
+                // クリティカル率は限界突破の際に1%ずつ上昇
+                charaData.criticalRate += 0.01f;
             }
             else
             {
-                // 通常は現在の戦闘力の2.5064%を足す
-                //combatPower += combatPower * 0.025064f;
+                // 通常は現在の各ステータスの2.5%を足す
+                charaData.attackPower += (int)Math.Round(charaData.attackPower * 0.025f, 0, MidpointRounding.AwayFromZero);
+                charaData.defencePower += (int)Math.Round(charaData.defencePower * 0.025f, 0, MidpointRounding.AwayFromZero);
+                charaData.hp += (int)Math.Round(charaData.hp * 0.025f, 0, MidpointRounding.AwayFromZero);
             }
         }
 
-        // TODO 適切な値を返す
-        return -1;
+        // 上で求めた各ステータスから、戦闘力を計算。
+        // 戦闘力 = 攻撃力+防御力+補正後HP+補正後クリティカル率
+        charaData.combatPower = (int)Math.Round(charaData.attackPower + charaData.defencePower + (charaData.hp * HP_MODIFIRE) + (charaData.attackPower + charaData.attackPower * 0.5) / ATTACK_MODIFIER * charaData.criticalRate, 0, MidpointRounding.AwayFromZero);
     }
 }
--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public class GameData : AbstractSingleton<GameData>
 {
+    [System.Serializable]
+    public class OwnedCharaData
+    {
+        public CharaName name;
+        public int level;
 
+        public int combatPower;
+        public int attackPower;
+        public int defencePower;
+        public int hp;
+        public float criticalRate;
+    }
+
+    public List<OwnedCharaData> ownedCharaDataList = new();
 }
