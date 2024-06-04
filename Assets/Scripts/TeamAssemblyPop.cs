75871cf732e8b2c7d04e66f6dc64602fd263aff6
sakura1783 <sarukaniumai@gmail.com>
2024-06-05 02:23:07 +0900

    TeamAssemblyPopゲームオブジェクト・スクリプト追加、CharaButton.cs追加

--- Assets/Scripts/TeamAssemblyPop.cs
+++ Assets/Scripts/TeamAssemblyPop.cs
@@ -1,8 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TeamAssemblyPop : MonoBehaviour
 {
+    [SerializeField] private Button btnFight;
 
+
+    
 }
