ba2b2ceb83a5d0406132a0467953742c44ee42e1
sakura1783 <sarukaniumai@gmail.com>
2024-06-21 18:18:56 +0900

    SkillManger.csを作成。スキルの基本的な処理を記述する。キャラの各スクリプトからこのクラスのメソッドを利用してスキルの挙動を作る

---
+++ Assets/Scripts/Character/SkillManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// スキルの基本的な処理を記述するクラス
+/// 各キャラのスクリプト内で組み合わせや引数を指定し、スキルの挙動を作る
+/// </summary>
+public static class SkillManager
+{
+
+}
